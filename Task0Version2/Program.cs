9961b3b644c07cb18075913f4249a0bdbe132f95
Ваше имя <garaevaea@mail.ru>
2022-03-11 17:00:25 +0300

    Add Task0Version2

---
+++ Task0Version2/Program.cs
@@ -0,0 +1,17 @@
+﻿int maxNumber = -50;
+
+for (int i = Math.Abs(maxNumber); i >= maxNumber; i -= 10)
+{
+    Console.WriteLine(i);
+}
+
+/*
+i -= x -> i = i - x
+i *= x -> i = i * x
+i += x -> i = i + x
+i /= x -> i = i / x
+i %= -> i = i % x
+i++ -> i = i + 1
+i-- -> i = i - 1
+Math.Abs - число по модулю
+*/
