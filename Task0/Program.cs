b3e4d2e5ac84aa604b08fddb62089c8c94b6337b
Ваше имя <garaevaea@mail.ru>
2022-03-11 16:33:10 +0300

    Add Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа
+
+int GetMaxLetterOfNumber(int number)
+{
+    int result = number % 10;
+    int letter2 = number / 10;
+    if (result < letter2)
+    {
+        result = letter2;
+    }
+    return result;
+}
+
+int randNumber = new Random().Next(10, 100);
+int result = GetMaxLetterOfNumber(randNumber);
+
+Console.WriteLine($"У числа {randNumber} наибольшая цифра {result}");
+
+Console.WriteLine("У числа " + randNumber + " наибольшая цифра " + result);
