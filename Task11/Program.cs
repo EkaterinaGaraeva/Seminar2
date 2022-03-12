55ac8a14793e8d737d76e1233ebd5efd04d6ddab
Ваше имя <garaevaea@mail.ru>
2022-03-12 20:54:00 +0300

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+
+int number = new int();
+Console.Write("Введите трехзначное число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+number = number / 10;
+int letter2 = number % 10;
+
+Console.WriteLine($"Вторая цифра числа: {letter2}");
